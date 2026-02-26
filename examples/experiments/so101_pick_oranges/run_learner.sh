c42548e111894caab7fc6b3124df9e3192832776
incalos <incalos@foxmail.com>
2026-02-26 17:00:58 +0800

    修复一些问题

--- examples/experiments/so101_pick_oranges/run_learner.sh
+++ examples/experiments/so101_pick_oranges/run_learner.sh
@@ -1,3 +1,3 @@
 cd ../../../
 source .venv/bin/activate
-uv run examples/train_rlpd.py --exp_name=so101_pick_oranges --checkpoint_path=examples/experiments/so101_pick_oranges/checkpoints --ip=192.168.11.91 --learner 
+uv run examples/train_rlpd.py --exp_name=so101_pick_oranges --checkpoint_path=examples/experiments/so101_pick_oranges/checkpoints --ip=0.0.0.0 --learner 

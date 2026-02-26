ce8ceb323db0f1a038d9b731c4e31e6a6bdbd732
incalos <incalos@foxmail.com>
2026-02-26 11:02:20 +0800

    pynput在无头环境下允许

--- examples/experiments/so101_pick_oranges/run_learner.sh
+++ examples/experiments/so101_pick_oranges/run_learner.sh
@@ -1,3 +1,5 @@
 cd ../../../
 source .venv/bin/activate
+export DISPLAY=:99
+Xvfb :99 -screen 0 1024x768x16 &
 uv run examples/train_rlpd.py --exp_name=so101_pick_oranges --checkpoint_path=examples/experiments/so101_pick_oranges/checkpoints --ip=localhost --learner 
